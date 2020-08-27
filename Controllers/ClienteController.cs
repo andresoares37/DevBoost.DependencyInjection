99681b7325cbaf3bb80164528bc69208d02c2c0f
Andre Soares <andresoares37@gmail.com>
2020-08-26 21:32:53 -0300

    first commit

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Application.Domain;
+using Application.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevBoost.DepedencyInjection.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        private readonly IClienteService _cliente;
+
+        public ClienteController(IClienteService cliente)
+        {
+            _cliente = cliente;
+        }
+        
+        [HttpGet]
+        [Produces("application/json")]
+        [ProducesResponseType(typeof(Cliente), (int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType(typeof(Cliente), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(Cliente), (int)HttpStatusCode.Unauthorized)]
+        [ProducesResponseType(typeof(Cliente), (int)HttpStatusCode.NotFound)]
+        [ProducesResponseType(typeof(Cliente), (int)HttpStatusCode.InternalServerError)]
+        public ActionResult GetCliente()
+        {
+            var clientes = _cliente.Get();
+            return Ok(clientes);
+        }
+
+        
+        
+        [HttpPost]
+        [Produces("application/json")]
+        [ProducesResponseType(typeof(Cliente), (int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType(typeof(Cliente), (int)HttpStatusCode.Created)]
+        [ProducesResponseType(typeof(Cliente), (int)HttpStatusCode.Unauthorized)]
+        [ProducesResponseType(typeof(Cliente), (int)HttpStatusCode.UnprocessableEntity)]
+        [ProducesResponseType(typeof(Cliente), (int)HttpStatusCode.InternalServerError)]
+        public ActionResult Post(Cliente cliente)
+        {
+            _cliente.Add(cliente);
+            return Ok(cliente);
+        }
+    }
+}
---
+++ Controllers/PedidosController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevBoost.DepedencyInjection.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PedidosController : ControllerBase
+    {
+        private readonly IPedidoService _pedidoService;
+        public PedidosController(IPedidoService pedidoService)
+        {
+            _pedidoService = pedidoService;
+        }
+
+        [HttpPost]
+        public ActionResult PostPedido(Pedidos pedido)
+        {
+            _pedidoService.Add(pedido);
+            return Ok(pedido);
+        }
+    }
+}
